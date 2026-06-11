bd35f01f215b9d006c5841f45e9f8ab29a199ab5
Dmitry Kostylev <smileyfvce@yandex.ru>
2026-06-11 20:00:34 +0300

    feat: начальная настройка подключения к DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password',
+      database: 'skill_swap',
+      entities: [],
+      synchronize: false
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
