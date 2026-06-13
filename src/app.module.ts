5bd1e6620fb204043c199671174ce8522b2313fc
Yaneart <artemskek@gmail.com>
2026-06-13 10:52:22 +0300

    add dbConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './config/db.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
-      username: process.env.DB_USERNAME || 'admin',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'skill_swap',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dbConfig()), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
