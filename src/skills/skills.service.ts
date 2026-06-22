0b679291ee23867524e04472ee9403e861348fe2
levrozhkov-dev <levrozhkov89@gmail.com>
2026-06-22 21:24:32 +0300

    feat(skills): add skill delete

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
+import { Skill } from './entities/skill.entity';
 
 @Injectable()
 export class SkillsService {
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillsRepository: Repository<Skill>,
+  ) {}
+
   create(createSkillDto: CreateSkillDto) {
     return 'This action adds a new skill';
   }
@@ -20,7 +32,21 @@ export class SkillsService {
     return `This action updates a #${id} skill`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} skill`;
+  async remove(id: string, userId: string): Promise<Skill> {
+    const skill = await this.skillsRepository.findOne({
+      where: { id },
+      relations: ['owner'],
+    });
+
+    if (!skill) {
+      throw new NotFoundException('Навык не найден');
+    }
+
+    if (skill.owner.id !== userId) {
+      throw new ForbiddenException('Можно удалить только свой навык');
+    }
+
+    await this.skillsRepository.remove(skill);
+    return skill;
   }
 }
