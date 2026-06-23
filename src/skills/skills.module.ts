daf720f31b127ddbbd576424e41b484a79ea1386
Dmitry Kostylev <smileyfvce@yandex.ru>
2026-06-24 00:35:28 +0300

    fix: убрал дубликаты импортов и поле categoryId в методе create класса SkillsService

--- src/skills/skills.module.ts
+++ src/skills/skills.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SkillsService } from './skills.service';
 import { SkillsController } from './skills.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Skill } from './entities/skill.entity';
 
 @Module({
