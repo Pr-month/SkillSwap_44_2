fc54196bc151ecaae04fbc9e356411ec02cea15d
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-06-24 22:26:53 +0500

    feat(requests): requests resource generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig, TDbConfig } from './config/db.config';
 import { SkillsModule } from './skills/skills.module';
 import { configLoaders } from './config/configuration';
+import { RequestsModule } from './requests/requests.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { configLoaders } from './config/configuration';
     UsersModule,
     AuthModule,
     SkillsModule,
+    RequestsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
