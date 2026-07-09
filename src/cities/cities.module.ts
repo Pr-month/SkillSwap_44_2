a8391805df3f917d4d4e043f23d31a7498cca906
Vazhdaichik95 <vazhdaichik95@yandex.ru>
2026-07-09 10:21:20 +0300

    Task 170. Fix CitiesModule
    Приложение успешно стартует. CitiesModule теперь содержит TypeOrmModule.forFeature([City]) в imports.

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { City } from './entities/city.entity';
 import { CitiesService } from './cities.service';
 import { CitiesController } from './cities.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([City])],
   controllers: [CitiesController],
   providers: [CitiesService],
 })
